32a0d1439997d18e804c55255ee5287b7b5d5751
kilin <rin.ueno@cpc.ait.kyushu-u.ac.jp>
2023-06-11 01:01:11 +0900

    230610: Add preview image at select image

--- src/components/modules/SelectFile.js
+++ src/components/modules/SelectFile.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
-import { Button, Input } from "@mui/material";
+import { Button, Box } from "@mui/material";
 import FolderOutlinedIcon from "@mui/icons-material/FolderOutlined";
+import styled from "styled-components";
 
 const SelectFile = (props) => {
   const [preview, setPreview] = useState("");
@@ -12,9 +13,48 @@ const SelectFile = (props) => {
     inputRef.current.click();
   };
 
+  const previewImage = (e) => {
+    const files = e.target.files;
+    if (files?.length === 0) {
+      return;
+    }
+    const reader = new FileReader();
+
+    reader.readAsDataURL(files[0]);
+    // reader.onload = (e) => {
+    //   setPreview(e.target?.result);
+    // };
+    reader.addEventListener("load", (e) => {
+      console.log(e.target.result);
+      setPreview(e.target?.result);
+    });
+  };
+
   return (
     <>
-      <input hidden ref={inputRef} type="file" accept="image/*" />
+      <input
+        hidden
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+        onChange={previewImage}
+      />
+      <div>
+        {/* {TODO:画像比率は保持したい} */}
+        {preview && (
+          <Box
+            component="img"
+            sx={{
+              height: 300,
+              width: 300,
+              maxHeight: { xs: 400, md: 400 },
+              maxWidth: { xs: 400, md: 400 },
+            }}
+            src={preview}
+            alt=""
+          />
+        )}
+      </div>
       <Button
         variant="outlined"
         size="large"
