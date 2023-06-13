79c3251539db43923665c8b8d2d374f10113ca00
kilin <rin.ueno@cpc.ait.kyushu-u.ac.jp>
2023-06-14 02:14:16 +0900

    js to ts

--- backend/function/index.js
+++ backend/function/index.js
@@ -1,4 +1,7 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const server = require("backend-modules/server");
+
+const app = express();
---
+++ backend/function/utils/server/server.js
@@ -0,0 +1,5 @@
+module.export = (app) => {
+  app.listen(3000, () => {
+    console.log("App Started");
+  });
+};
