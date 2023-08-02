609bb481bcf3f7518f87ab3c886ec6997db7831e
kilin <rin.ueno@cpc.ait.kyushu-u.ac.jp>
2023-08-03 02:10:49 +0900

    del unnessesary

--- backend/zip_layer.sh
+++ backend/zip_layer.sh
@@ -24,8 +24,5 @@ pip install -t ${dist_path} -r requirements.txt
 cd ${dist_path}
 zip ${zip_path} -r . || echo "zip Error"
 cd ${WORK_DIR}
-mv ${zip_path} .
+# mv ${zip_path} . # case: for use local check
 echo ${zip_path} # e.g:/tmp/tmp.whZEKhn6ky/python-layer-2023-08-02-01-56.zip
-
-## OKだね。mvはする必要が無いからここはまたいじる必要があるね。echo zip_pathでいいと思う。
-## 正確に言うと、これはzipを作っただけだね。
